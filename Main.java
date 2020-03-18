6a3866f07be1a873ff5114544d869a676fa1f984
elan9799 <61521729+elan9799@users.noreply.github.com>
2020-03-18 17:37:33 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,14 +3,14 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int array[] = new int[200];
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < array.length; i++) {
             int a = (int) (Math.random() * 200 - 100);
             array[i] = a;
         }
-        int array2[] = new int[200];
+        int array2[] = new int[array.length];
 
         int x = 0;
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] > 0) {
                 array2[x] = array[i];
                 x++;
@@ -21,7 +21,7 @@ public class Main {
 
         int sum = 0;
         int sum2 = 0;
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (i % 2 == 0) {
                 sum = sum + array[i];
             } else {
@@ -33,7 +33,7 @@ public class Main {
         System.out.println("Расзность = " + (sum - sum2));
 
 
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] > 0) {
                 System.out.println("NO");
                 System.out.println(i + " " + array[i]);
@@ -45,7 +45,7 @@ public class Main {
         }
 
         int hh = -100;
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (hh > array[i]) {
                 System.out.println("NOPE");
                 break;
@@ -73,5 +73,31 @@ public class Main {
         }
         System.out.println(Arrays.toString(array));
 
+
+        System.out.println("второй максимум = " + array[array.length - 2]);
+
+        String stringArray[] = new String[20];
+        int g = 0;
+        int a = 10;
+        boolean f = false;
+        for (int i = 0; i < array.length; i++) {
+            if (a == array[i]) {
+                f = true;
+                stringArray[g] = Integer.toString(i);
+                g += 1;
+            }
+        }
+        if (f == true) {
+            System.out.println("заданное число находится в массиве по индексам = " + Arrays.toString(stringArray));
+        } else {
+            System.out.println("нет совпадений");
+        }
+
+        g = 0;
+        for (int i = 0; i < array.length; i++) {
+            g = g + array[i];
+        }
+        g = g / (array.length - 1);
+        System.out.println("среднее значение элементов в массиве = " + g);
     }
 }
